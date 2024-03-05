c5f65b56da03b7499b19565450d472047980af39
Ryan McIntyre <rmcintyre598@gmail.com>
2024-03-05 14:43:01 -0500

    messed with the pages

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -2,7 +2,7 @@
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
+    <ul className="justify-content-end nav nav-tabs ">
       <li className="nav-item">
         <a
           href="#about"
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,7 +3,8 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-     put this on the page
+     I am a junior developer. I took the UCF Full Stack Developer Bootcamp. We learned a diverse amount of languages in that course. It started out with HTML, CSS, and Javascript. I learned
+     how to uses databases like DBeaver with SQL, and MongoDB with no Sql. I have become very familiar with node.js and React.
       </p>
     </div>
   );
